0b5cbfee59320d7815e26596459e0138cca71e01
Mohammed Hafiz <hafiz408.mohammed@gmail.com>
2021-07-05 14:53:48 +0530

    User authentication added, linked with mongoose DB, sessions included and draft cupid page added

--- app.js
+++ app.js
@@ -1,27 +1,110 @@
+
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const alert = require('alert'); 
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended : true}));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get("/",function(req,res){
+app.use(session({
+  secret: "Our own Cupid.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/cupidDB", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    username: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function(req, res){
     res.sendFile(__dirname+"/mainpage.html");
-})
+});
 
-app.post("/",function(req,res){
-    res.sendFile(__dirname+"/mainpage.html");
-})
-
-app.post("/login",function(req,res){
+app.get("/login", function(req, res){
     res.sendFile(__dirname+"/login.html");
-})
+});
 
-app.post("/signup",function(req,res){
+app.get("/register", function(req, res){
     res.sendFile(__dirname+"/sign_up.html");
-})
+});
+
+app.get("/cupid", function(req, res){
+  if (req.isAuthenticated()){
+    res.render("cupid");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/register", function(req, res){
+
+        User.register({username: req.body.username}, req.body.password, function(err, user){
+            if (err) {
+              console.log(err);
+              alert("Mail ID already exists. Try logging in !!");
+              res.redirect("/register");
+            } else {
+                passport.authenticate("local")(req, res, function(){
+                    res.redirect("/cupid");
+                  });
+            }
+          });
+
+});
+
+app.post("/login", function(req, res){
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/cupid");
+      });
+    }
+  });
+
+});
+
 
 app.listen(9200,function(){
     console.log("Server running at port 9200");
